a5cddff734adb62aec8526c084c5cf3e705ecbed
jayshawn <jayshawn@qq.com>
2018-05-22 18:42:19 +0800

    Update MyappApplication.java

--- src/main/java/myapp/MyappApplication.java
+++ src/main/java/myapp/MyappApplication.java
@@ -14,6 +14,9 @@ public class MyappApplication {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 	
+	//这条配置不再支持：spring.mvc.view.viewClass=org.springframework.web.servlet.view.JstlView
+	//并且使用spring.mvc.prefix spring.mvc.suffix配置不起作用，猜测是spring boot版本较高，可能在旧版本中支持，
+	//但由于在新版本中进一步减弱了对jsp的支持，因此不再自动配置ViewResolver对象，导致jsp解析失败
 	@Bean
 	public ViewResolver getViewResolver(){
 	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
